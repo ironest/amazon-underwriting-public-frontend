c0bc9e9a6db24ec8a6e6b0b063150f2dcbfab047
alkex1 <alikentwell@hotmail.com>
2020-02-03 23:53:40 +1100

    fixed navbar, some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,19 @@ class NavBar extends Component {
     render() {
         return (
             <div className="nav-div" >
+                <div className="left">
                 <Link to="/">
                     <button>HomePage</button>
                 </Link>
                 <Link to="/childcare">
                     <button>Childcare</button>
                 </Link>
+                </div>
+                <div className="right">
+                <Link to="/contact" >
+                    <button >Contact Us</button>
+                </Link>
+                </div>
             </div>
         )
     }
